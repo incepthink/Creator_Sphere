8271f4b1d7658288249e5e935af7cd2544513fa5
incepthink <91987044+incepthink@users.noreply.github.com>
2024-09-22 03:58:13 +0530

    Update Claim NFT page

--- src/app/minted/page.tsx
+++ src/app/minted/page.tsx
@@ -13,6 +13,12 @@ const MintedPage = () => {
       <div className="flex items-center justify-center">
         <CardContainer className="inter-var">
           <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+            <CardItem
+              translateZ="50"
+              className="text-2xl text-center font-bold text-neutral-600 dark:text-white"
+            >
+              Taaha Nizam&apos;s NFT
+            </CardItem>
             <CardItem translateZ="100" className="w-full mt-4">
               <Image
                 src={Animation}
